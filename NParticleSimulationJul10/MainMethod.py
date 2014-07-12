bd5bf19f9afc854cf473d2f2d22d9ac30e45ebb1
ssddttaa <ssddttaa@gmail.com>
2014-07-12 16:35:47 -0400

    Debugging, Code now runs

--- NParticleSimulationJul10/MainMethod.py
+++ NParticleSimulationJul10/MainMethod.py
@@ -4,71 +4,66 @@ from particleClass import particleClass
 
 particleList = []
 time = 0
-deltat = 100
+deltat = .01
 G = 6.67E-11
 
 def createNewParticle(radius, mParticle, pos, velocity):
-    newParticle = particleClass(radius, mParticle, pos, velocity,(random.uniform(0,1),random.uniform(0,1),random.uniform(0,1)))
+    newParticle = particleClass(radius, mParticle, pos, velocity,(1,0,0))
+    print "newParticle"+str(newParticle.particle_radius)
     return newParticle
 
+def createDefinedParticle(radius, mParticle, pos, velocity):
+    tempParticle = createNewParticle(radius,mParticle,pos,velocity)
+    print "particle list size:"+str(len(particleList))
+    particleList.append(tempParticle)
+    print "particle list size after:"+str(len(particleList))
 def createRandomParticles(radiusRangeStart,radiusRangeEnd, mParticleRangeStart, mParticleRangeEnd, posXStart,posXEnd,posYStart,posYEnd,posZStart,posZEnd,velocityXStart,velocityXEnd,velocityYStart,velocityYEnd,velocityZStart,velocityZEnd,numParticles):
     for x in range(0,numParticles):
-        print "now producing particle with radius range:"+str(radiusRangeStart)+"-"+str(radiusRangeEnd)
+        print "radus range start and end:"+str(radiusRangeStart)+str(radiusRangeEnd)
         newRadius = random.uniform(radiusRangeStart,radiusRangeEnd)
-        print "new radius:"+str(newRadius)
+        print "new radius"+str(newRadius)
         newMass = random.uniform(mParticleRangeStart, mParticleRangeEnd)
         
         newPosX = random.uniform(posXStart,posXEnd)
         newPosY = random.uniform(posYStart,posYEnd)
         newPosZ = random.uniform(posZStart,posZEnd)
         
-        newPosVector = vector((newPosX,newPosY,newPosZ))
-        print "new position vector:"+str(newPosVector)
+        newPosVector = vector((newPosX/1E10,newPosY/1E10,newPosZ/1E10))
         
         newVelocityX = random.uniform(velocityXStart,velocityXEnd)
         newVelocityY = random.uniform(velocityYStart,velocityYEnd)
         newVelocityZ = random.uniform(velocityZStart,velocityZEnd)
         
         newVelocityVector = vector((newVelocityX,newVelocityY,newVelocityZ))
-        print "new velocity vector:"+str(newVelocityVector)
         tempParticle = createNewParticle(newRadius,newMass,newPosVector,newVelocityVector)
         particleList.append(tempParticle)
-        print "finished creating the particle"
-        print "length of particleList"+str(len(particleList))
-        print "values of particle list radiusVector:"+str(tempParticle.particle_pos)
 
-
-"""
-    
-    radius range for particles: 1E6 - 7.5E7
-    
-    mass range for particles: 2E22 - 2E27
-    
-    radius range center of system: 4E5 - 5E8
-    
-    mass range center of system: 1.5E30 - 2.5E30
-    
-    velocity range for particles: 5E4 - 13E4
-    
-    magnitude range of position vector: 6E10 - 6E12 """
 
 createRandomParticles(1E6,7.5E7,2E22,2E27,6E10,6E12,6E10,6E12,6E10,6E12,5E4,13E4,5E4,13E4,5E4,13E4,1)
-createNewParticle(6.955E10,2.0E30,(0,0,0),(0,0,0))
+createDefinedParticle(6.955E10,2.0E30,(0,0,0),(0,0,0))
 
 def eulerIntegration(acceleration, particle):
     
     velocity = acceleration * deltat + particle.particle_velocity
-    position = particle.particle_velocity * deltat + particle.particle_pos
+    position = vector(particle.particle_velocity) * deltat + vector(particle.particle_pos)
     return position
 
 
 def getGravitationalAcceleration(particle1, particle2):
-    r = particle2.particle_pos - particle1.particle_pos
+    r = vector(particle2.particle_pos) - vector(particle1.particle_pos)
     accelerationParticle1 = ( G * particle2.particle_mass * norm(r) ) / mag2(r)
     return accelerationParticle1
 
+def drawSphere(ParticleClass):
+        print "drawing sphere with pos:"+str(ParticleClass.particle_pos)+"and radius:"+str(1.0E20*ParticleClass.particle_radius)
+        sphere(pos = ParticleClass.particle_pos, radius = 1.0E20*ParticleClass.particle_radius, color = ParticleClass.particle_color)
+
 while(time<10):
+    sleep(.1)
+    i = 0
     for i in range(0,len(particleList)):
+        tempParticle = particleList[i]
+        drawSphere(tempParticle)
         newAcceleration = vector((0,0,0))
         newPos = vector((0))
         for j in range (0,len(particleList)):
@@ -76,7 +71,7 @@ while(time<10):
                 newAcceleration = getGravitationalAcceleration(particleList[i],
                                                      particleList[j])
         newPos = eulerIntegration(newAcceleration,particleList[i])
-        tempParticle = particleList[i]
+        
         tempParticle.particle_pos = newPos
         particleList[i] = tempParticle
     time = time+deltat
--- NParticleSimulationJul10/particleClass.py
+++ NParticleSimulationJul10/particleClass.py
@@ -1,18 +1,16 @@
 from visual import*
 
-class particleClass(object):
+class particleClass:
     particle_radius = 0
     particle_mass = 0
     particle_pos = vector((0,0,0))
     particle_velocity = vector((0,0,0))
     particle_color = (0,0,0)
     def __init__(self,radius,mParticle,pos,velocity, color):
-        print "the constructor is being called with position vector:"+str(pos)
         self.particle_radius = radius
         self.particle_mass = mParticle
         self.particle_pos = pos
         self.particle_velocity = velocity
-        print "updated position vector:"+str(self.particle_pos)
     def updatePosition(mParticle, pos, velocity, color):
         particle_mass = mParticle
         particle_pos = pos
