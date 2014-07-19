101bf6ad338b876c433caae40293794d1b4702d6
ssddttaa <ssddttaa@gmail.com>
2014-07-19 11:49:03 -0400

    Sadat and Doug's code with Runge/Kutta.
    Must be debugged, currently does not work.

--- NParticleSimulationJul10/MainMethod_Sadat_Doug.py
+++ NParticleSimulationJul10/MainMethod_Sadat_Doug.py
@@ -4,7 +4,7 @@ from particleClass import particleClass
 
 particleList = []
 time = 0
-deltat = 100000
+deltat = 1000
 G = 6.67E-11
 
 <<<<<<< HEAD
@@ -98,21 +98,21 @@ createDefinedParticle(6.955E9,2.0E30,(0,0,0),(0,0,0))
 #Earth
 createDefinedParticle(6.3781E9,5.9E24,(1.5E11,0,0),(0,3.0E4,0))
 #Mercury
-createDefinedParticle(2.4397E9,328.5E21,(57.91E9,0,0),(0,48.9E3,0))
+#createDefinedParticle(2.4397E9,328.5E21,(57.91E9,0,0),(0,48.9E3,0))
 #Venus
-createDefinedParticle(6.045E9,4.8676E24,(1.08E11,0,0),(0,35.02E3,0))
+#createDefinedParticle(6.045E9,4.8676E24,(1.08E11,0,0),(0,35.02E3,0))
 #Mars
-createDefinedParticle(3.389E9,0.64174E24,(2.279E11,0,0),(0,24.07E3,0))
+#createDefinedParticle(3.389E9,0.64174E24,(2.279E11,0,0),(0,24.07E3,0))
 #Jupiter
-createDefinedParticle(71.408E9,1898.3E24,(7.785E11,0,0),(0,13.06E3,0))
+#createDefinedParticle(71.408E9,1898.3E24,(7.785E11,0,0),(0,13.06E3,0))
 #Saturn
-createDefinedParticle(60.1965E9,568.3E24,(8.907E11,0,0),(0,9.68E3,0))
+#createDefinedParticle(60.1965E9,568.3E24,(8.907E11,0,0),(0,9.68E3,0))
 #Uranus
-createDefinedParticle(25.362E9,86.81E24,(2.877E12,0,0),(0,6.80E3,0))
+#createDefinedParticle(25.362E9,86.81E24,(2.877E12,0,0),(0,6.80E3,0))
 #Neptune
-createDefinedParticle(24.622E9,102.4E24,(4.503E12,0,0),(0,5.43E3,0))
+#createDefinedParticle(24.622E9,102.4E24,(4.503E12,0,0),(0,5.43E3,0))
 #Pluto
-createDefinedParticle(1.191E11,1.31E22,(4.44E12,0,0),(0,4.67E3,0))
+#createDefinedParticle(1.191E11,1.31E22,(4.44E12,0,0),(0,4.67E3,0))
 #createDefinedParticle(6.3781E9,5.9E30,(-1.7E11,0,0),(0,-3.0E4,0))
 
 
@@ -124,25 +124,61 @@ def eulerIntegration(acceleration, particle):
 	#print "new acceleration, new velocity, new position"+str(acceleration)+str(velocity)+str(position)
 	return position, velocity
 
-def runge_kutta(acceleration,particle):
-	kv1 = acceleration
-	kv2 = acceleration * deltat / 2 * kv1
-	kv3 = acceleration * deltat / 2 * kv2
-	kv4 = acceleration * deltat * kv3
-	#velocity = vector(0,0,0)
-	particle.velocity = vector(particle.velocity)
-	velocity = particle.velocity + (deltat / 6) * (kv1 + 2*kv2 + 2*kv3 + kv4)
+def getGravitationalAcceleration(particle1, particle2):
+	r = particle1.pos - particle2.pos
+	accelerationParticle1 = -1*( G * particle2.mass * norm(r) ) / mag2(r)
+	#print "acceleration of particle 1:"+str(accelerationParticle1)
+	return accelerationParticle1
 
-	kp1 = velocity * deltat + particle.pos
-	kp2 = velocity * deltat / 2 * kp1
-	kp3 = velocity * deltat / 2 * kp2
-	kp4 = acceleration + deltat * kp3
-	position = vector(0,0,0)
-	position = particle.pos + (deltat / 6) * (kp1 + 2*kp2 + 2*kp3 + kp4)
-	#print "new acceleration, new velocity, new position"+str(acceleration)+str(velocity)+str(position)
+def getGravitationalAccelerationWithXVec(xVector,particle1,particle2):
+	acceleration = -1*( G * particle2.mass * norm(xVector) ) / mag2(xVector)
+	return acceleration
+	
+def runge_kutta(acceleration,particle1,particle2):
+	kv1 = acceleration
+	print "kv1:"+str(kv1)
+	kv2X = particle1.pos+((deltat/2.0)*(particle1.velocity+(deltat*kv1/2.0)))
+	kv2 = getGravitationalAccelerationWithXVec(kv2X,particle1,particle2)
+	print "kv2X:"+str(kv2X)
+	print "kv2:"+str(kv2)
+	kv3X = particle1.pos+((deltat/2.0)*(particle1.velocity+(deltat*kv2/2.0)))
+	kv3 = getGravitationalAccelerationWithXVec(kv3X,particle1,particle2)
+	print "kv3X:"+str(kv3X)
+	print "kv3:"+str(kv3)
+	kv4X = particle1.pos+((deltat)*(particle1.velocity+(deltat*kv3)))
+	kv4 = getGravitationalAccelerationWithXVec(kv4X,particle1,particle2)
+	print "kv4X:"+str(kv4X)
+	print "kv4:"+str(kv4)
+	velocity = particle1.velocity+(deltat/6.0)*(kv1+2*kv2+2*kv3+kv4)
+	print "velocity:"+str(velocity)
+	kp1 = particle1.velocity
+	kp2 = particle1.velocity+getGravitationalAccelerationWithXVec(particle1.pos+(deltat*kp1/2.0)*deltat/2.0,particle1,particle2)
+	kp3 = particle1.velocity+getGravitationalAccelerationWithXVec(particle1.pos+(deltat*kp2/2.0)*deltat/2.0,particle1,particle2)
+	kp4 = particle1.velocity+getGravitationalAccelerationWithXVec(particle1.pos+(deltat*kp3)*deltat,particle1,particle2)
+
+	position = particle1.pos+(deltat/6.0)*(kp1+2*kp2+2*kp3+kp4)
+
+	print "position:"+str(position)+"velocity:"+str(velocity)
+	
 	return position,velocity
 											
-	
+
+def updateParticleUsingEulerIntegration():
+    i = 0
+    for i in range(0,len(particleList)):
+        tempParticle = particleList[i]
+        newAcceleration = vector((0,0,0))
+        newPos = vector((0,0,0))
+        for j in range (0,len(particleList)):
+            if(not(i==j)):
+                newAcceleration += getGravitationalAcceleration(particleList[i],particleList[j])
+                tempParticle.pos,tempParticle.velocity = eulerIntegration(newAcceleration,particleList[i])
+        particleList[i].trail.append(pos=tempParticle.pos)
+        particleList[i] = tempParticle
+
+def updateParticleUsingRungeKuttaIntegration():
+    
+
 """p(x).velocity = p(x)acceleration * deltat + p(x).velocity
 p(x).position = p(x)velocity * deltat + p(x).position
 
@@ -162,30 +198,13 @@ p(x).position = p(x).position + deltat / 6 * (kp1 + 2*kp2 + 2*kp3 + kp4)"""
 				
 
 
-def getGravitationalAcceleration(particle1, particle2):
-	r = particle1.pos - particle2.pos
-	accelerationParticle1 = -1*( G * particle2.mass * norm(r) ) / mag2(r)
-	#print "acceleration of particle 1:"+str(accelerationParticle1)
-	return accelerationParticle1
+
 
 
 while(True):
 	rate(1000)
-	i = 0
-	for i in range(0,len(particleList)):
-
-		tempParticle = particleList[i]
-		newAcceleration = vector((0,0,0))
-		newPos = vector((0,0,0))
-		for j in range (0,len(particleList)):
-			if(not(i==j)):
-				newAcceleration += getGravitationalAcceleration(particleList[i],particleList[j])
-                
-		tempParticle.pos,tempParticle.velocity = eulerIntegration(newAcceleration,particleList[i])
-		particleList[i].trail.append(pos=tempParticle.pos)
-		particleList[i] = tempParticle
- 
-		#print "pos: %.8f,%.8f,%.8f"%((particleList[i].pos.x),(particleList[i].pos.y),(particleList[i].pos.z))
+	#updateParticleUsingEulerIntegration()
+	updateParticleUsingRungeKuttaIntegration()
 	time = time+deltat
 
 
